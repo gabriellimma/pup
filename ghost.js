74606706f95fe618050611c0843b5d8402d614b0
Bielzin42 <nickspoot@gmail.com>
2020-09-15 21:37:25 -0300

    lauching pup... :rocket:

--- ghost.js
+++ ghost.js
@@ -9,14 +9,26 @@ async function ghost() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://www.samsung.com/ca/accessibility/overview/');
+
+    const pageTwo = await browser.newPage();
+    await pageTwo.goto('https://www.samsung.com/ca/accessibility/overview/');
     
     const title =  await page.evaluate(() => {
         return document.querySelector('head > title').text
       });
 
+      const titleTwo =  await pageTwo.evaluate(() => {
+        return document.querySelector('head > title').text
+      });
    
+      if(title == titleTwo) {
+        console.log('equal');
+      } else {
+        console.log('non-equal');
+      }
 
     console.log(title)
+    console.log(titleTwo)
     await browser.close();
 
 }
